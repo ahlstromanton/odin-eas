0af3d5e5b06b9d3f23a0733ee191856a9d08bec4
anton <anton.l.ahlstrom@gmail.com>
2023-09-10 23:05:21 +0200

    grid functions

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+const requestedDivs = 16;
+
 function createGrid() {
-    numberOfDivs = 0;
-    requestedDivs = 16;
-    while (numberOfDivs < requestedDivs) {
-        const container = document.querySelector('#masterDivContainer');
-        const div = document.createElement('div');
-        div.setAttribute('class', 'square');
-        div.setAttribute('id', 'square-item' + numberOfDivs);
-        container.appendChild(div);
-        numberOfDivs++;
+    const container = document.getElementById('gridContainer');
+    for (let i = 0; i < requestedDivs; i++) {
+        createRow(container);
     }
 }
+
+function createRow(container) {
+    const flexrow = document.createElement('div');
+    flexrow.setAttribute('class', 'flexRow');
+    container.appendChild(flexrow);
+    createColumns(flexrow);
+}
+
+function createColumns(rowContainer) {
+    for (let i = 0; i < requestedDivs; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'square');
+        rowContainer.appendChild(div);
+    }
+}
+
+createGrid();
